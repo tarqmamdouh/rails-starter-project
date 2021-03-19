94f7f1f05d2e8794a9061988b8c833c9c98e0f3b
Tariq Mamdouh Ahmed <tarq.mamdouh1996@gmail.com>
2021-03-19 04:04:59 +0100

    Edited answers table to contain question reference

--- db/migrate/20210319022602_create_answers.rb
+++ db/migrate/20210319022602_create_answers.rb
@@ -1,7 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.integer :user_id, index: true, foreign_key: true
+      t.integer :user_id, foreign_key: true
+      t.integer :question_id, index: true, foreign_key: true
       t.text :body
       t.timestamps
     end
